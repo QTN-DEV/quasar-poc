f14f43d2377b66e5ec43e553c6371b2435e608da
raflihadiana <raflihadian24@gmail.com>
2024-11-25 05:08:55 +0000

    Update Skrip Linux

--- quasar-agent/host-agent/linux-installer.sh
+++ quasar-agent/host-agent/linux-installer.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Function to prompt for Wazuh Manager IP and Agent Name
+prompt_wazuh_details() {
+    read -p "Enter Wazuh Manager IP: " wazuh_manager_ip
+    read -p "Enter Wazuh Agent name: " wazuh_agent_name
+}
+
 # Function to install Wazuh Agent
 install_wazuh_agent() {
     echo "Installing Wazuh Agent..."
-    curl -sO https://packages.wazuh.com/4.x/apt/KEY.gpg
-    sudo apt-key add KEY.gpg
-    echo "deb https://packages.wazuh.com/4.x/apt/ stable main" | sudo tee /etc/apt/sources.list.d/wazuh.list
-    sudo apt-get update
-    sudo apt-get install wazuh-agent -y
+    wget https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.8.0-1_amd64.deb
+    sudo WAZUH_MANAGER="$wazuh_manager_ip" WAZUH_AGENT_NAME="$wazuh_agent_name" dpkg -i ./wazuh-agent_4.8.0-1_amd64.deb
     sudo systemctl daemon-reload
     sudo systemctl enable wazuh-agent
     sudo systemctl start wazuh-agent
@@ -44,28 +47,54 @@ install_suricata() {
     echo "Suricata installation completed."
 }
 
-# Function to configure Suricata
+get_homenet_ip() {
+    # Get the first non-loopback interface IP with subnet
+    ip -4 -o addr show | awk '!/ lo/{print $4; exit}'
+}
+
+# Function to download and set up Emerging Threats rules
+setup_rules() {
+    echo "Downloading and extracting Emerging Threats Suricata ruleset..."
+    cd /tmp/ && curl -LO https://rules.emergingthreats.net/open/suricata-6.0.8/emerging.rules.tar.gz
+    sudo tar -xvzf emerging.rules.tar.gz
+    sudo mkdir -p /etc/suricata/rules
+    sudo mv rules/*.rules /etc/suricata/rules/
+    sudo chmod 640 /etc/suricata/rules/*.rules
+    echo "Ruleset setup completed."
+}
+
+# Function to modify Suricata configuration
 configure_suricata() {
-    echo "Configuring Suricata to output logs in JSON format..."
-    sudo sed -i 's/^#\s*output-json:/output-json:/' /etc/suricata/suricata.yaml
-    sudo systemctl restart suricata
-    echo "Suricata configuration completed."
+    local homenet_ip
+    homenet_ip=$(get_homenet_ip)
+    if [ -z "$homenet_ip" ]; then
+        echo "Error: Unable to determine HOME_NET IP. Exiting."
+        exit 1
+    fi
+
+    echo "Modifying /etc/suricata/suricata.yaml with HOME_NET: $homenet_ip"
+    sudo sed -i "s|HOME_NET:.*|HOME_NET: \"$homenet_ip\"|" /etc/suricata/suricata.yaml
+    sudo sed -i '/rule-files:/,/af-packet:/ s|^[[:space:]]*-.*rules|  - "*.rules"|' /etc/suricata/suricata.yaml
+
+    # Get the primary non-loopback network interface
+    local interface
+    interface=$(ip -o -4 route show to default | awk '{print $5}')
+
+    if [ -z "$interface" ]; then
+        echo "Error: Unable to determine network interface. Exiting."
+        exit 1
+    fi
+
+    echo "Setting af-packet interface to: $interface"
+    sudo sed -i "/af-packet:/,/interface:/ s|^[[:space:]]*interface:.*|  - interface: $interface|" /etc/suricata/suricata.yaml
+    echo "Suricata configuration updated."
 }
 
-# Function to configure Suricata
-configure_yara() {
-    echo "Configuring Suricata to output logs in JSON format..."
-    sudo sed -i 's/^#\s*output-json:/output-json:/' /etc/suricata/suricata.yaml
+# Function to restart Suricata
+restart_suricata() {
+    echo "Restarting Suricata service..."
     sudo systemctl restart suricata
-    echo "Suricata configuration completed."
-}
-
-# Function to configure Suricata
-configure_clamav() {
-    echo "Configuring Suricata to output logs in JSON format..."
-    sudo sed -i 's/^#\s*output-json:/output-json:/' /etc/suricata/suricata.yaml
-    sudo systemctl restart suricata
-    echo "Suricata configuration completed."
+    echo "Suricata service restarted."
 }
 
 # Function to configure Wazuh Agent for Suricata logs
@@ -81,75 +110,66 @@ EOF'
     echo "Wazuh Agent configuration for Suricata completed."
 }
 
+# Function to configure Wazuh Agent for ClamAV logs
 configure_wazuh_for_clamav() {
-    echo "Configuring Wazuh Agent to monitor Suricata logs..."
+    echo "Configuring Wazuh Agent to monitor ClamAV logs..."
     sudo bash -c 'cat << EOF >> /var/ossec/etc/ossec.conf
 <localfile>
-  <log_format>json</log_format>
-  <location>/var/log/suricata/eve.json</location>
+  <log_format>syslog</log_format>
+  <location>/var/log/clamav/clamav.log</location>
 </localfile>
 EOF'
     sudo systemctl restart wazuh-agent
-    echo "Wazuh Agent configuration for Suricata completed."
+    echo "Wazuh Agent configuration for ClamAV completed."
 }
 
+# Function to configure Wazuh Agent for YARA logs
 configure_wazuh_for_yara() {
-    echo "Configuring Wazuh Agent to monitor Suricata logs..."
+    echo "Configuring Wazuh Agent to monitor YARA logs..."
     sudo bash -c 'cat << EOF >> /var/ossec/etc/ossec.conf
 <localfile>
-  <log_format>json</log_format>
-  <location>/var/log/suricata/eve.json</location>
+  <log_format>syslog</log_format>
+  <location>/var/log/yara/yara.log</location>
 </localfile>
 EOF'
     sudo systemctl restart wazuh-agent
-    echo "Wazuh Agent configuration for Suricata completed."
+    echo "Wazuh Agent configuration for YARA completed."
 }
 
-# Function to perform default installation
-install_all() {
-    install_wazuh_agent
+# Function to install all add-ons
+install_add_ons() {
     install_clamav
     install_yara
     install_suricata
-    configure_suricata
     configure_clamav
     configure_yara
+    configure_suricata
+    setup_rules
+    configure_suricata
+    restart_suricata
     configure_wazuh_for_suricata
     configure_wazuh_for_clamav
     configure_wazuh_for_yara
-    }
+}
+
 # Display simplified menu
 show_menu() {
     echo "Choose an installation option:"
-    echo "1) Default Installation (Wazuh Agent, ClamAV, YARA, Suricata)"
-    echo "2) Install Wazuh Agent with ClamAV"
-    echo "3) Install Wazuh Agent with YARA"
-    echo "4) Install Wazuh Agent with Suricata"
-    echo "5) Exit"
-    read -p "Enter your choice [1-5]: " choice
+    echo "1) Install Wazuh Agent only"
+    echo "2) Install Wazuh Agent with all Add-Ons (ClamAV, YARA, Suricata)"
+    echo "3) Exit"
+    read -p "Enter your choice [1-3]: " choice
     case $choice in
         1)
-            install_all
+            prompt_wazuh_details
+            install_wazuh_agent
             ;;
         2)
+            prompt_wazuh_details
             install_wazuh_agent
-            install_clamav
-            configure_clamav
-            configure_wazuh_for_clamav
+            install_add_ons
             ;;
         3)
-            install_wazuh_agent
-            install_yara
-            configure_yara
-            configure_wazuh_for_yara
-            ;;
-        4)
-            install_wazuh_agent
-            install_suricata
-            configure_suricata
-            configure_wazuh_for_suricata
-            ;;
-        5)
             echo "Exiting..."
             exit 0
             ;;
